8c1067e97428bf72a23bb574b937845578681de0
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-05-15 00:05:59 -0600

    instances created, need to develope

--- index.js
+++ index.js
@@ -9,7 +9,36 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// get data to json file
+let primary_data = fs.readFileSync('users.json');
+let users = json.parse(primary_data);
 
+// routes
+app.get('/newUser', (req, res) => {
+    res.render('newUser', { user: {} });
+});
+app.get('/', (req, res) => {
+    res.redirect('userList');
+});
+
+// what will happend when user clicks the input button ???????
+app.post('/createUser', (req, res) => {
+
+})
+
+app.get('/userList', (req, res) => {
+});
+
+app.get('/editList/:userId', (req, res) => {
+    
+});
+app.post('/editList/:userId', (req, res) => {
+
+});
+
+
+app.get('/delete:userId', (req, res) => {
+});
 
 //initialize the server    --change to PORT????--
 app.listen(3000, () => {
