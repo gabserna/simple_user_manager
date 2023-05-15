017187eec2794bf7c9194e6e8f1d61ff22fcc727
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-05-14 23:44:04 -0600

    middleware added

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function userNew() {
-    let user = user + userNew
-};
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const uuidd = require('uuidd');
+
+// this are the middlewares
+app.use(express.static('public'));  //static page
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+
+//initialize the server    --change to PORT????--
+app.listen(3000, () => {
+    console.log('Server up and listening on port 3000');
+})
