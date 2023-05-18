0e05d9934f9d6467fb6f386224742d4b813b5e03
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-05-17 23:51:55 -0600

    code simplified trying dry

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const uuidd = require('uuidd');
-
+const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 // this are the middlewares
-app.use(express.static('public'));  //static page
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public')); // for serving static assets'
 app.set('views', './views');
+app.set('view engine', 'pug');
 
-// get data to json file
-let primary_data = fs.readFileSync('users.json');
-let users = JSON.parse(primary_data);
+// Global variable to store users
+let users = [];
+
+// Function to load users from 'users.json'
+function loadUsers() {
+  const data = fs.readFileSync('users.json');
+  users = JSON.parse(data);
+}
+
+// Function to save users to 'users.json'
+function saveUsers() {
+  const data = JSON.stringify(users);
+  fs.writeFileSync('users.json', data);
+}
+
+// Load initial set of users
+loadUsers();
 
 // routes
 app.get('/newUser', (req, res) => {
-    res.render('newUser', { user: {} });
+  res.render('newUser', { user: {} });
 });
 
 app.get('/', (req, res) => {
-    res.redirect('/userList');
+  res.redirect('/userList');
 });
 
-// what will goint to happend when user clicks the input button ???????
 app.post('/createUser', (req, res) => {
-    const user = {
-        userId: uuidd.v4(),
-        username: req.body.username,
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-    };
-    users.users.push();
-    let data = JSON.stringify(users);
-    fs.writeFileSync('users.json', data);
-    res.redirect('/userList');
+  const user = {
+    userId: uuid.v4(),
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  };
+  users.users.push(user);
+  saveUsers();
+  res.redirect('/userList');
 });
 
 app.get('/userList', (req, res) => {
-    let primary_data = fs.readFileSync('users.json');
-    let users = JSON.parse(primary_data);
-    res.render();
+  loadUsers();
+  res.render('userList', { users: users.users });
 });
 
 app.get('/editList/:userId', (req, res) => {
-    let primary_data = fs.readFileSync('users.json');
-    let users = JSON.parse(primary_data);
-    res.render();
+  const user = findUserById(req.params.userId);
+  res.render('editList', { user });
 });
+
 app.post('/editList/:userId', (req, res) => {
-    let primary_data = fs.readFileSync('users.json');
-    let users = JSON.parse(primary_data);
-    res.render();
+  const user = findUserById(req.params.userId);
+  user.username = req.body.username;
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.age = req.body.age;
+  saveUsers();
+  res.redirect('/userList');
 });
 
-app.get('/delete:userId', (req, res) => {
-    let primary_data = fs.readFileSync('users.json');
-    let users = JSON.parse(primary_data);
-    res.render();
+app.get('/delete/:userId', (req, res) => {
+  users.users = users.users.filter(user => user.userId !== req.params.userId);
+  saveUsers();
+  res.redirect('/userList');
 });
 
-//initialize the server    --change to PORT????--
+// Function to find a user by ID
+function findUserById(userId) {
+  return users.users.find(user => user.userId == userId);
+}
+
+//initialize the server
 app.listen(3000, () => {
-    console.log('Server up and listening on port 3000');
-})
+  console.log('Server listening on port 3000');
+});
--- notes.md
+++ notes.md
@@ -1,5 +1,9 @@
 npm i --save-dev nodemon
 git init
 echo 'node_modules/' >.gitignore
-npm init -y
 
+npm init -y
+npm i express
+npm i fs
+npm i uuid
+npm i pug
